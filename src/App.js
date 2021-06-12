27bdbe2267f02a54cf5014ec5e997c4472b82544
Naim Zaman <egnaimur@gmail.com>
2021-06-12 21:18:38 +0600

    Render Multiple Component From An Array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const nayoks = ['Jasim','DeepJol','BappRaz','Omar Sani','Alamgir'];
+	const nayoks01 = [
+		{
+			name:'Jasim',
+		 	age:'56'
+		},
+		{
+			name:'DeepJol',
+		 	age:'61'
+		},
+		{
+			name:'BappRaz',
+		 	age:'15'
+		},
+		{
+			name:'Omar Sani',
+		 	age:'35'
+		}
+	];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<header className="App-header">
+			<h1 style={{color:'red'}}>Mapping Part One</h1>
+			{
+				nayoks.map(item => <Nayoks name={item}></Nayoks>)
+			}
+			<h1 style={{color:'red'}}>Mapping Part Two</h1>
+			{
+				nayoks01.map(item => <Nayoks name={item.name} age={item.age}></Nayoks>)
+			}
+
+		</header>
+	</div>
   );
 }
+
+//======>>Create Component [Start]
+function Nayoks(props){
+	// console.log(props.name);
+	const nayoksStyle = {
+		border:'2px solid purple',
+		margin:'20px',
+		borderRadius:'7px'
+	}
+	return (
+		<div style={nayoksStyle}>
+			<h1>Ami Nayok- <span style={{color:'purple'}}>{props.name}</span></h1>
+			<h3>I have done 5 movies in {props.age} years</h3>
+		</div>
+	)	
+}
+//======>>Create Component [End]
 
 export default App;
